147eae6752cfa852aea789a62bb50e97a30b1167
muhammadatoqeer <muhammadanstoqeer@gmail.com>
2025-01-01 20:02:48 +0500

    functions approach (Tabs)

--- Tabs/index.js
+++ Tabs/index.js
@@ -6,21 +6,31 @@ const buttons5 = document.getElementById('button5');
 
 const information = document.getElementById('information')
 
-button1.onclick = function (e) {
-    information.innerHTML = `${button1.innerHTML} was pressed`
+// button1.onclick = function (e) {
+//     information.innerHTML = `${button1.innerHTML} was pressed`
+// }
+
+// buttons2.onclick = function (e) {
+//     information.innerHTML = `${buttons2.innerHTML} was pressed`
+// }
+// buttons3.onclick = function (e) {
+//     information.innerHTML = `${buttons3.innerHTML} was pressed`
+// }
+
+// buttons4.onclick = function (e) {
+//     information.innerHTML = `${buttons4.innerHTML} was pressed`
+// }
+
+// buttons5.onclick = function (e) {
+//     information.innerHTML = `${buttons5.innerHTML} was pressed`
+// }
+
+function clicked(arg) {
+    let info = document.getElementById(arg)
+    information.innerHTML = `${info.innerHTML} was pressed`
 }
 
-buttons2.onclick = function (e) {
-    information.innerHTML = `${buttons2.innerHTML} was pressed`
-}
-buttons3.onclick = function (e) {
-    information.innerHTML = `${buttons3.innerHTML} was pressed`
-}
-
-buttons4.onclick = function (e) {
-    information.innerHTML = `${buttons4.innerHTML} was pressed`
-}
-
-buttons5.onclick = function (e) {
-    information.innerHTML = `${buttons5.innerHTML} was pressed`
-}
+// buttons2.onclick = clicked('button2');
+// buttons3.onclick = clicked('button3');
+// buttons4.onclick = clicked('button4');
+// buttons5.onclick = clicked('button5');
