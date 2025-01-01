6a7d3ffffa4bb25bb8947f8e9ed3efa8d28c5de3
muhammadatoqeer <muhammadanstoqeer@gmail.com>
2025-01-01 14:46:02 +0500

    Tabs (WordPress) implemented

---
+++ Tabs/index.js
@@ -0,0 +1,26 @@
+const button1 = document.getElementById('button1');
+const buttons2 = document.getElementById('button2');
+const buttons3 = document.getElementById('button3');
+const buttons4 = document.getElementById('button4');
+const buttons5 = document.getElementById('button5');
+
+const information = document.getElementById('information')
+
+button1.onclick = function (e) {
+    information.innerHTML = `${button1.innerHTML} was pressed`
+}
+
+buttons2.onclick = function (e) {
+    information.innerHTML = `${buttons2.innerHTML} was pressed`
+}
+buttons3.onclick = function (e) {
+    information.innerHTML = `${buttons3.innerHTML} was pressed`
+}
+
+buttons4.onclick = function (e) {
+    information.innerHTML = `${buttons4.innerHTML} was pressed`
+}
+
+buttons5.onclick = function (e) {
+    information.innerHTML = `${buttons5.innerHTML} was pressed`
+}
